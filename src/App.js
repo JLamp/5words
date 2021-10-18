0ae774118e77107591c718f23f2edbe41ae27416
JLamp <jlampron@wistia.com>
2021-10-18 01:54:55 -0400

    finalize init

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import './App.css';
 import { useState } from 'react';
 import { syllable } from 'syllable';
 import { transparentize } from 'polished';
-import { uuid } from 'uuidv4';
 
 const PageContainer = styled.div`
 width: 100%;
 height: 100%;
+display: flex;
+flex-direction: column;
+width: 100%;
+align-items: center;
 `;
 
 const AppHeader = styled.div`
 max-width: 960px;
-margin: 24px auto;
-`;
-
-const MainContentContainer = styled.div`
-display: flex;
-width: 100%;
-justify-content: center;
+margin: 24px 0;
 `;
 
 const MainContentArea = styled.div`
@@ -83,7 +80,8 @@ margin-left: 2px;
 margin-right: 2px;
 border-radius: 2px;
 line-height: 1.5;
-background: ${({syllables}) => transparentize(.75, getBackgroundColor(syllables))}
+background: ${({syllables}) => transparentize(.75, getBackgroundColor(syllables))};
+opacity: ${({repition}) => 1 - ( repition < 4 ? (.15 * repition) : (.15 * 4))};
 `;
 
 
@@ -104,26 +102,64 @@ function App() {
   
   const [text, changeText] = useState(defaultTextArray);
   
+  function count(sentence){
+    return syllable(sentence)
+  }
 
+  function findSize(sentence){
+    var sentenceSize = count(sentence);
+    if(sentenceSize < 1){
+      sentenceSize = "s0";
+    }else if (sentenceSize < 5){
+      sentenceSize = "s4";
+    } else if (sentenceSize < 11) {
+      sentenceSize = "s10";
+    } else if (sentenceSize < 21){
+      sentenceSize = "s20";
+    } else if (sentenceSize < 31){
+      sentenceSize = "s30";
+    } else if (sentenceSize < 41){
+      sentenceSize = "s40"
+    } else if (sentenceSize < 51){
+      sentenceSize = "s50"
+    } else if (sentenceSize < 71){
+      sentenceSize = "s70"
+    } else if (sentenceSize < 91){
+      sentenceSize = "s90"
+    } else {
+      sentenceSize = "sx";
+    }
+    return sentenceSize;
+  }
+
+  function checkRepition(paragraph, sentence, index){
+    const sentenceSize = findSize(sentence);
+    var repition = 0;
+    for (var i = (index - 1); i > -1; i--){
+      console.log(paragraph[i]);
+      if (findSize(paragraph[i]) === sentenceSize){
+        repition = repition + 1;
+      } else {
+        break;
+      }
+    }
+    return repition;
+  }
   
   function handleChange({ currentTarget }) {
     const text = makeArray(currentTarget.value)
     changeText(currentTarget.value.length > 0 ? text : defaultTextArray);
   }
   
-  
-  
   return (
     <PageContainer>
     <AppHeader>
-    <h1>Rhythm Writing</h1>
+    <h1>This sentence has 5 words...</h1>
     </AppHeader>
-    <MainContentContainer>
     <MainContentArea>
     <TextInput type="textarea" placeholder={defaultText} name="name" onChange={handleChange}></TextInput>
-    <OutputText>{text.map((paragraph) => <Paragraph key={uuid()}>{paragraph.map((sentence) => <Sentence key={uuid()} syllables={syllable(sentence)}>{sentence.trim()}</Sentence>)}</Paragraph>)}</OutputText>
+    <OutputText>{text.map((paragraph, index) => <Paragraph key={index}>{paragraph.map((sentence, index) => <Sentence key={index} repition={checkRepition(paragraph, sentence, index)} syllables={count(sentence)}>{sentence.trim()}</Sentence>)}</Paragraph>)}</OutputText>
     </MainContentArea>
-    </MainContentContainer>
     </PageContainer>
     );
   }
